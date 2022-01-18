3302edd8012c6cbef2c7f83ff9f41335c57373a8
Mars <houjianchao1204@gmail.com>
2022-01-18 11:16:28 +0800

    Add flutter boost

---
+++ android/app/src/main/kotlin/com/example/boost_getx/application/MApplication.kt
@@ -0,0 +1,47 @@
+package com.example.boost_getx.application
+
+import android.app.Application
+import android.content.Intent
+import android.widget.Toast
+import com.idlefish.flutterboost.FlutterBoost
+import com.idlefish.flutterboost.FlutterBoostDelegate
+import com.idlefish.flutterboost.FlutterBoostRouteOptions
+import com.idlefish.flutterboost.containers.FlutterBoostActivity
+import io.flutter.embedding.android.FlutterActivityLaunchConfigs
+import io.flutter.embedding.engine.FlutterEngine
+
+
+/**
+ * Created by Mars on 2022/1/17 16:35.
+ */
+class MApplication : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+        FlutterBoost.instance().setup(this, object : FlutterBoostDelegate {
+            override fun pushNativeRoute(options: FlutterBoostRouteOptions) {
+                //这里根据options.pageName来判断你想跳转哪个页面，这里简单给一个
+                Toast.makeText(this@MApplication, options.pageName(), Toast.LENGTH_SHORT).show()
+//                val intent = Intent(
+//                    FlutterBoost.instance().currentActivity(),
+//                    YourTargetAcitvity::class.java
+//                )
+//                FlutterBoost.instance().currentActivity()
+//                    .startActivityForResult(intent, options.requestCode())
+            }
+
+            override fun pushFlutterRoute(options: FlutterBoostRouteOptions) {
+                val intent: Intent =
+                    FlutterBoostActivity.CachedEngineIntentBuilder(FlutterBoostActivity::class.java)
+                        .backgroundMode(FlutterActivityLaunchConfigs.BackgroundMode.transparent)
+                        .destroyEngineWithActivity(false)
+                        .uniqueId(options.uniqueId())
+                        .url(options.pageName())
+                        .urlParams(options.arguments())
+                        .build(FlutterBoost.instance().currentActivity())
+                FlutterBoost.instance().currentActivity().startActivity(intent)
+            }
+        }) { engine: FlutterEngine? -> }
+    }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,114 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_boost/flutter_boost.dart';
 
 void main() {
+  CustomFlutterBinding();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class CustomFlutterBinding extends WidgetsFlutterBinding
+    with BoostFlutterBinding {}
+
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  /// 由于很多同学说没有跳转动画，这里是因为之前exmaple里面用的是 [PageRouteBuilder]，
+  /// 其实这里是可以自定义的，和Boost没太多关系，比如我想用类似iOS平台的动画，
+  /// 那么只需要像下面这样写成 [CupertinoPageRoute] 即可
+  /// (这里全写成[MaterialPageRoute]也行，这里只不过用[CupertinoPageRoute]举例子)
+  ///
+  /// 注意，如果需要push的时候，两个页面都需要动的话，
+  /// （就是像iOS native那样，在push的时候，前面一个页面也会向左推一段距离）
+  /// 那么前后两个页面都必须是遵循CupertinoRouteTransitionMixin的路由
+  /// 简单来说，就两个页面都是CupertinoPageRoute就好
+  /// 如果用MaterialPageRoute的话同理
+
+  Map<String, FlutterBoostRouteFactory> routerMap = {
+    '/': (RouteSettings settings, String uniqueId) {
+      return CupertinoPageRoute(
+          settings: settings,
+          builder: (_) {
+            Map<String, Object> map = settings.arguments ?? {};
+            var data = map['data'] ?? "";
+            return MainPage(
+              data: data,
+            );
+          });
+    },
+    'simplePage': (settings, uniqueId) {
+      return CupertinoPageRoute(
+          settings: settings,
+          builder: (_) {
+            var map = settings.arguments as Map<String, Object> ?? {};
+            var data = map['data'] ?? "";
+            return SimplePage(
+              data: data,
+            );
+          });
+    },
+  };
+
+  Route<dynamic> routeFactory(RouteSettings settings, String uniqueId) {
+    FlutterBoostRouteFactory func = routerMap[settings.name];
+    if (func == null) {
+      return null;
+    }
+    return func(settings, uniqueId);
+  }
+
+  Widget appBuilder(Widget home) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: home,
+      debugShowCheckedModeBanner: true,
+
+      ///必须加上builder参数，否则showDialog等会出问题
+      builder: (_, __) {
+        return home;
+      },
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return FlutterBoostApp(
+      routeFactory,
+      appBuilder: appBuilder,
+      initialRoute: "simplePage",
     );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({dynamic data});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Main Page')),
+    );
+  }
+}
+
+class SimplePage extends StatelessWidget {
+  const SimplePage({dynamic data});
+
+  static void _incrementCounter() {
+    print("object");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(child: Text('SimplePage')),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        ));
   }
 }
